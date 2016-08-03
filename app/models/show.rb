3a5492b2d3f56a155bbd65a2ef69204fc5ca2228
ozPop <ozziepopovas@gmail.com>
2016-08-03 14:35:44 -0400

    add changes

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,29 +1,29 @@
 class Show < ActiveRecord::Base
-  def highest_rating
-    Show.maximum(:rating)
+  def self.highest_rating
+    maximum(:rating)
   end
 
-  def most_popular_show
-
+  def self.most_popular_show
+    find_by(rating: highest_rating)
   end
 
-  def lowest_rating
-
+  def self.lowest_rating
+    minimum(:rating)
   end
 
-  def least_popular_show
-
+  def self.least_popular_show
+    find_by(rating: lowest_rating)
   end
 
-  def ratings_sum
-
+  def self.ratings_sum
+    sum(:rating)
   end
 
-  def popular_shows
-
+  def self.popular_shows
+    where("rating > ?", 5)
   end
 
-  def shows_by_alphabetical_order
-
+  def self.shows_by_alphabetical_order
+    order(:name)
   end
 end
